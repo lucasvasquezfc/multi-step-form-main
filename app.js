11a3702a2f8b3c2887a61f0ea948edf72ec1a1a1
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-11-13 13:05:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const circles = document.querySelectorAll('.num');
 const box = document.querySelector('.content_box_items');
 const pages = document.querySelectorAll('.pages');
 
+let currentActive = 0;
+
 function display() {
   pages.forEach((page, index) => {
     if (index == currentActive) {
@@ -18,48 +20,12 @@ function display() {
     } else {
       prev.style.visibility = 'visible';
     }
-    if (currentActive == 4) {
+    if (currentActive == 3) {
       next.style.visibility = 'hidden';
     } else {
       next.style.visibility = 'visible';
     }
   });
-}
-
-function changeDisplay() {
-  if (currentActive == 0) {
-    prev.style.visibility = 'hidden';
-    firstPage.style.display = 'block';
-  } else {
-    prev.style.visibility = 'visible';
-    firstPage.style.display = 'none';
-  }
-
-  if (currentActive == 1) {
-    secondPage.style.display = 'block';
-  } else {
-    secondPage.style.display = 'none';
-  }
-
-  if (currentActive == 2) {
-    thirdPage.style.display = 'block';
-  } else {
-    thirdPage.style.display = 'none';
-  }
-
-  if (currentActive == 3) {
-    fourthPage.style.display = 'block';
-  } else {
-    fourthPage.style.display = 'none';
-  }
-
-  if (currentActive == 4) {
-    fifthPage.style.display = 'block';
-    next.style.visibility = 'hidden';
-  } else {
-    fifthPage.style.display = 'none';
-    next.style.visibility = 'visible';
-  }
 }
 
 function update() {
@@ -78,7 +44,6 @@ function update() {
     }
 
     display();
-    // changeDisplay();
   });
 }
 
